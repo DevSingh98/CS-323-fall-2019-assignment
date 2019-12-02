2526264d14af4803209b65fcfc9612af07b1951b
Anthony Torres <AnthonyJTorres58@gmail.com>
2019-12-01 21:02:47 -0500

    initial edits to league code

--- LeagueOfPatience.java
+++ LeagueOfPatience.java
@@ -40,7 +40,33 @@ public class LeagueOfPatience {
     // Feel free to borrow code from any of the existing methods.
     // You will find the getNextQuestTime method and the minutesBetween method helpful.
     // You can also make new helper methods.
+    
+    //startTimes are datatype Date, make Date Array of startTimes.
+    Date[] startTimes = new Date[durations.length];
+    //First startTime for array will be given startTime
+    startTimes[0] = startTime;
+    Boolean[] processed = new Boolean[durations.length];
+    
+    
+    for (int v = 0; v < numVertices; v++){
+        times[v] = Integer.MAX_VALUE;
+        processed[v] = false;
+    }
+    
 
+    times[S] = 0;
+    
+    for(int count = 0; count < numVertices - 1; count++) {
+        int u = findNextToProcess(times, processed);
+        processed[u] = true;
+        
+        for (int v=0; v < numVertices; v++){
+            if (!processed[v] && graph[u][v] != 0 && 
+            times[u] != integer.MAX_VALUE && times[u]+graph[u][v] < times[v])
+                    times[v] = times[u] + graph[u][v];
+        }
+    }
+    
     printShortestTimes(times);
 
     // Extra Credit: Code below to print the suggested play path i.e. "2, 4, 3, 5"
